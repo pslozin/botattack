8b2475ff20bf43dccb8a5706138400d1d79d982a
philipslozin@gmail.com <pslozin@gmail.com>
2023-03-22 12:53:17 -0400

    BOTS

---
+++ fizzbuzz.js
@@ -0,0 +1,22 @@
+let i;
+
+for (let i = 0; i < 50; i++) 
+{ 
+
+    if (i % 3 == 0)
+    {
+        console.log("FIZZ")
+    }
+    
+
+    else if (i % 5 === 0)
+    {
+    console.log("BUZZ")
+
+    }
+
+    else
+    {
+       console.log(i) 
+    }
+  }
---
+++ makingdec.js
@@ -0,0 +1,11 @@
+let botAttack1 = 20
+let botAttack2 = 30
+
+if(botAttack1 > botAttack2)
+{
+    console.log("Bot1 more power than Bot2")
+}
+else
+{
+    console.log("Bot2 more power than Bot1")
+}
